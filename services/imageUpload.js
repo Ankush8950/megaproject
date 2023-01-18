abb6e668ed02411d844f553914527f8fc69d9dcf
Ankush kumar <69650280+Ankush8950@users.noreply.github.com>
2023-01-18 10:59:02 +0000

    aws image service

---
+++ services/imageUpload.js
@@ -0,0 +1,21 @@
+import s3 from "../config/S3.config"
+
+
+export const s3FillUpload = async ({bucketName,key,body,contentType})=>{
+    return await s3.upload({
+        Bucket: bucketName,
+        Key:key,
+        Body:body,
+        ContentType:contentType
+    })
+    .promise()
+}
+
+
+export const deleteFile =async ({bucketName,key}) =>{
+    return await s3.deleteObject({
+        Bucket:bucketName,
+        Key:key
+    })
+    .promise()
+}
